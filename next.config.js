fcc75b61af010967b350706f61cd959b2e41fe16
Fajar <kucaykojek@gmail.com>
2024-01-04 12:53:40 +0700

    chore: webpack external canvas

--- next.config.js
+++ next.config.js
@@ -9,6 +9,10 @@ const nextConfig = {
       }
     ]
   }
+  // webpack: (config) => {
+  //   config.externals = [...config.externals, 'canvas']
+  //   return config
+  // }
 }
 
 module.exports = nextConfig
